aa014a4f93791d5c0c70c2fd5a21afcb506f38ba
gaoyaqin <yaqin-gao@foxmail.com>
2020-07-08 17:30:48 +0800

    异常时关闭线程池,未测试

--- src/main/java/com/ponder/m3u8java/M3u8Main.java
+++ src/main/java/com/ponder/m3u8java/M3u8Main.java
@@ -11,9 +11,19 @@ public class M3u8Main {
     static String host = "https://baidu.com-ok-baidu.com/20191011/15291_b2977bc7/1000k/hls/";
 
     public static void main(String[] args) throws Exception {
+        addExceptionHandler();
         InputStream index = ClassLoader.getSystemResourceAsStream("test.m3u8");
         M3u8 m3u8 = new M3u8(index, host);
         m3u8.download();
+    }
+
+    private static void addExceptionHandler() {
+        Thread.currentThread().setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                DownLoadRunnable.executorService.shutdown();
+            }
+        });
     }
 
 }
