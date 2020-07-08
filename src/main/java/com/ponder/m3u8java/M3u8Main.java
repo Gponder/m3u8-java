5e908b36b9d7ba3f4bbac7df4769bbb9b86b427e
gaoyaqin <yaqin-gao@foxmail.com>
2020-07-08 15:36:32 +0800

    入口

--- src/main/java/com/ponder/m3u8java/M3u8Main.java
+++ src/main/java/com/ponder/m3u8java/M3u8Main.java
@@ -1,6 +1,7 @@
 package com.ponder.m3u8java;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @auth ponder
@@ -10,18 +11,10 @@ import java.io.*;
 public class M3u8Main {
     static String host = "https://baidu.com-ok-baidu.com/20191011/15291_b2977bc7/1000k/hls/";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         InputStream index = ClassLoader.getSystemResourceAsStream("test.m3u8");
         M3u8 m3u8 = new M3u8(index, host);
-        m3u8.parse();
         m3u8.downloadBodies();
-
-        File f = new File("C:\\Users\\gh\\Downloads\\6e8563db012000000.ts");
-        FileInputStream fis = new FileInputStream(f);
-        byte[] bytes = new byte[fis.available()];
-        fis.read(bytes);
-        String s = new String(bytes,"UTF-8");
-//        m3u8.getKey();
     }
 
 }
